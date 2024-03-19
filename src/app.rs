ad0a467ad2a5873e213b0ee45c4e97b6ccd6620b
Nate Sanchez <30782279+natesanchez314@users.noreply.github.com>
2024-03-19 16:29:50 -0500

    create an example layout

--- src/app.rs
+++ src/app.rs
@@ -2,6 +2,8 @@
 #[serde(default)]
 pub struct MyApp {
 	label: String,
+	terminal_out: String,
+	terminal_in: String,
 	#[serde(skip)]
 	value: f32
 }
@@ -10,6 +12,8 @@ impl Default for MyApp {
 	fn default() -> Self {
 		Self {
 			label: "Hello World!".to_owned(),
+			terminal_in: "Terminal in".to_owned(),
+			terminal_out: "Terminal out".to_owned(),
 			value: 2.7,
 		}
 	}
@@ -33,21 +37,53 @@ impl eframe::App for MyApp {
 		egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
 			egui::menu::bar(ui, |ui| {
 				ui.menu_button("File", |ui| {
+					if ui.button("Open File...").clicked() {}
+					if ui.button("Open Folder...").clicked() {}
 					if ui.button("Quit").clicked() {
 						ctx.send_viewport_cmd(egui::ViewportCommand::Close);
 					}
 				});
+				ui.menu_button("Edit", |_ui| {});
 				ui.add_space(16.0);
 				egui::widgets::global_dark_light_mode_buttons(ui);
 			});
 		});
-
-		egui::CentralPanel::default().show(ctx, |ui| {
-			ui.heading("eframe template");
-			ui.horizontal(|ui| {
-				ui.label("Write something!");
-				ui.text_edit_singleline(&mut self.label);
+		egui::TopBottomPanel::bottom("bottom_panel").resizable(true).show(ctx, |ui| {
+			ui.vertical_centered_justified(|ui| {
+				egui::ScrollArea::vertical().show(ui, |ui| {
+					ui.add(
+						egui::TextEdit::multiline(&mut self.terminal_out),
+					);
+				});
+				ui.add(
+					egui::TextEdit::singleline(&mut self.terminal_in),
+				);
 			});
+		});
+		egui::SidePanel::left("test").min_width(1.0).show(ctx, |ui| {
+			ui.heading("Folders");
+			egui::ScrollArea::both().stick_to_bottom(true).auto_shrink(false).show(ui, |ui| {
+				ui.collapsing("Example Folder", |ui| {
+					ui.collapsing("Example Subfolder", |ui| {
+						if ui.selectable_label(false, "Example File 1").clicked() {}	
+					});
+					if ui.selectable_label(false, "Example File 2").clicked() {}	
+				});
+			});
+		});
+		egui::CentralPanel::default().show(ctx, |ui| {
+			ui.horizontal_top(|ui| {
+				ui.available_size();
+				ui.vertical(|ui| {
+					ui.horizontal(|ui| {
+						if ui.selectable_label(false, "Example File").clicked() {}	
+					});
+					ui.add_sized(
+					ui.available_size(),
+						egui::TextEdit::multiline(&mut self.label).code_editor(),
+					);
+				});
+			});	
 		});
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use eframe::egui;
 fn main() -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([800.0, 600.0])
+            .with_inner_size([1280.0, 720.0])
             .with_min_inner_size([400.0, 300.0]),
         ..Default::default()
     };
